ecbd48d13a2e79db6e620f765248f9fd2a56e2f4
Mikhail Fesenko <proggga@gmail.com>
2016-10-17 20:02:20 +0300

    Added first route, and view with json

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+""" Views file """
+#from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+import json
+
+def index(request):
+    """ default index method """
+    response_data = { 'message' : 'This is my api' }
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
